3b140f69dda0b756568ad6bcd13fe7e819be7fa1
ilwsm <ua.andrey@rocketmail.com>
2024-03-09 02:02:53 +0200

    Update DbStatsApplication.java

--- src/main/java/org/kinocat/db_stats/DbStatsApplication.java
+++ src/main/java/org/kinocat/db_stats/DbStatsApplication.java
@@ -1,21 +1,15 @@
 package org.kinocat.db_stats;
 
-import com.mongodb.client.MongoCollection;
-import org.bson.Document;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
 @EnableCaching
 public class DbStatsApplication implements CommandLineRunner {
-    @Autowired
-    private MongoTemplate mongo;
 
     public static void main(String[] args) {
         SpringApplication.run(DbStatsApplication.class, args);
